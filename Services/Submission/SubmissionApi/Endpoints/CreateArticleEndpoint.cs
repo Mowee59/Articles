28e9809f2b8a81f8f0a21629ebcb71fe0684f932
Aniss <contact@aniss.dev>
2026-03-02 15:09:28 +0100

    feat(submission): add CreateArticle endpoint skeleton

---
+++ Services/Submission/SubmissionApi/Endpoints/CreateArticleEndpoint.cs
@@ -0,0 +1,17 @@
+﻿namespace Submission.Api.Endpoints;
+
+    public static class CreateArticleEndpoint
+    {
+        public static void Map(this IEndpointRouteBuilder app)
+        {
+            app.MapPost("api/articles", async () => { })
+                .RequireAuthorization(policy => policy.RequireRole("AUT"))
+                .WithName("CreateArticle")
+                .WithTags("Articles")
+                .Produces(StatusCodes.Status201Created)
+                .ProducesProblem((StatusCodes.Status401Unauthorized))
+                .ProducesValidationProblem((StatusCodes.Status400BadRequest));
+
+        }
+    }
+
