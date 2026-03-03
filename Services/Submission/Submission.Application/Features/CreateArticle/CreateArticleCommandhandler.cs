5331ca11f60d39c512349c3c2a69f84a5de3bc1b
Aniss <contact@aniss.dev>
2026-03-03 12:45:39 +0100

    feat(submission): implement create article command handler

--- Services/Submission/Submission.Application/Features/CreateArticle/CreateArticleCommandhandler.cs
+++ Services/Submission/Submission.Application/Features/CreateArticle/CreateArticleCommandhandler.cs
@@ -1,14 +1,20 @@
 ﻿using Articles.Abstractions;
 using MediatR;
+using Submission.Domain.Entities;
+using Submission.Persistence.Repositories;
 
 
 namespace Submission.Application.Features.CreateArticle;
 
-internal class CreateArticleCommandHandler : IRequestHandler<CreateArticleCommand, IdResponse>
+internal class CreateArticleCommandHandler(Repository<Journal> _journalRepository) : IRequestHandler<CreateArticleCommand, IdResponse>
 {
-    public Task<IdResponse> Handle(CreateArticleCommand request, CancellationToken cancellationToken)
+    public async Task<IdResponse> Handle(CreateArticleCommand command, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var journal = await _journalRepository.FindByIdAsync(command.JournalId); // TODO - Throw not found exception if journal is not found
+        var article = journal.CreateArticle(command.Title, command.ArticleType, command.Scope);
+        await _journalRepository.SaveChangesAsync(ct);
+
+        return new IdResponse(article.Id);
     }
 }
 
